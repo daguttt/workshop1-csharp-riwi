095a4f225085ff78b288419a0693421d66636d19
daguttt <dagutmu667@gmail.com>
2024-07-04 09:52:08 -0500

    feat: solve until the 7th exercise

--- workshop_3_loops/Program.cs
+++ workshop_3_loops/Program.cs
@@ -37,29 +37,39 @@ adivine el número. Muestra un mensaje indicando si el número ingresado es dema
 alto, demasiado bajo o correcto. Continúa pidiendo al usuario que adivine hasta que
 adivine correctamente usando un ciclo do-while.
 */
-Random random = new();
-int randomNumber = random.Next(1, 100 + 1);
-bool isGuessed = false;
-do
-{
-  Console.Write("Adivina el número generado entre 1 y 100: ");
-  int guessedNumber = Convert.ToInt32(Console.ReadLine());
-  if (guessedNumber == randomNumber)
-  {
-    Console.WriteLine($"¡Adivinaste el número! ({randomNumber})");
-    isGuessed = true;
-  }
+// Random random = new();
+// int randomNumber = random.Next(1, 100 + 1);
+// do
+// {
+//   Console.Write("Adivina el número generado entre 1 y 100: ");
+//   int guessedNumber = Convert.ToInt32(Console.ReadLine());
+//   if (guessedNumber == randomNumber)
+//   {
+//     Console.WriteLine($"¡Adivinaste el número! ({randomNumber})");
+//     break;
+//   }
 
-  if (guessedNumber > randomNumber) Console.WriteLine($"El número esta por debajo de {guessedNumber}");
-  else if (guessedNumber < randomNumber) Console.WriteLine($"El número está por encima de {guessedNumber}");
-  Console.WriteLine();
-}
-while (!isGuessed);
+//   if (guessedNumber > randomNumber) Console.WriteLine($"El número esta por debajo de {guessedNumber}");
+//   else if (guessedNumber < randomNumber) Console.WriteLine($"El número está por encima de {guessedNumber}");
+//   Console.WriteLine();
+// }
+// while (true);
 
 /*
 5. Escribe un programa que solicite al usuario un número entero positivo e imprima todos los
 números impares desde ese número hasta 1 usando un ciclo while.
 */
+// Console.WriteLine();
+// Console.Write("Ingresa un número entero positivo: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number >= 1)
+// {
+//   for (int i = 1; i <= number; i++)
+//   {
+//     if (i % 2 != 0) Console.WriteLine(i);
+//   }
+// }
+// else Console.WriteLine("El número debe ser 1 o mayor.");
 
 
 /*
@@ -67,11 +77,45 @@ números impares desde ese número hasta 1 usando un ciclo while.
 es "secreta", sigue pidiendo al usuario que ingrese la contraseña hasta que sea correcta
 usando un ciclo while.
 */
+// Console.WriteLine();
+// Console.Write("Ingresa una contraseña: ");
+// bool isGuessed = false;
+// while (!isGuessed)
+// {
+//   var password = Console.ReadLine();
+//   if (string.IsNullOrWhiteSpace(password) || !password.Equals("secreta", StringComparison.CurrentCultureIgnoreCase))
+//   {
+//     Console.WriteLine("Contraseña inválida. Intentalo de nuevo");
+//     continue;
+//   }
+
+//   Console.WriteLine("¡Contraseña correcta!");
+//   isGuessed = true;
+// }
 
 /*
 7. Escribe un programa que solicite al usuario un número entero positivo e imprima la
 secuencia de Fibonacci hasta ese número usando un ciclo for.
 */
+try
+{
+  Console.Write("Ingresa un número entero positivo: ");
+  uint number = uint.Parse(Console.ReadLine() ?? string.Empty);
+
+  List<ulong> fib = [0, 1];
+  for (ulong i = 0; i < number - 1; i++)
+  {
+    if (fib[^2] + fib[^1] > number) break;
+    fib.Add(fib[^2] + fib[^1]);
+  };
+  for (int i = 0; i < fib.Count; i++) Console.WriteLine($"{i + 1}. {fib[i]}");
+
+}
+catch (Exception)
+{
+  Console.WriteLine("El número debe ser positivo");
+  throw;
+}
 
 /*
 8. Escribe un programa que solicite al usuario un número entero positivo e imprima si el
